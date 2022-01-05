f9216824153f0b2dbadf3ad9d44f0f48a58ed45b
FlutLab.io <>
2022-01-05 01:39:14 +0000

    Autosave

--- lib/views/produto.dart
+++ lib/views/produto.dart
@@ -15,8 +15,8 @@ class Produto extends StatelessWidget {
           children: <Widget>[
             Image.network(
               imagemProduto!,
-              width: MediaQuery.of(context).size.width * 0.35,
-              height: MediaQuery.of(context).size.height * 0.50,
+              width: MediaQuery.of(context).size.width * 0.15,
+              height: MediaQuery.of(context).size.height * 0.20,
             ),
             Text(labelProduto!),
             Text('R\$ ${price!}'),
